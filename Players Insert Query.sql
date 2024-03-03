f2b86a7ed714b1f176e3b1857ce2993e9f7fdeee
sriv303 <abi.vemulapalli@gmail.com>
2024-03-03 18:02:52 +0000

    readded players, merged allrounder and removed duplicates

--- Players Insert Query.sql
+++ Players Insert Query.sql
@@ -1,15 +1,16 @@
---insert into players
-select distinct null,striker,batting_team,0,1 
-from odi_match_data 
+--insert into players(id, name,country,isBatsman,isBowler)
+select distinct null,striker,batting_team,1,0 
+from odi_match_data2 
 where batting_team not in ('Papua New Guinea',
 'United Arab Emirates',
 'United Arab Emirates',
 'Oman',
 'United States of America')
-group by striker,batting_team having sum(runs_off_bat)>500 
+group by striker,batting_team having sum(runs_off_bat)>300 
 
---insert into players
-select distinct null,bowler,bowling_team,1,0
+
+--insert into players(id, name,country,isBatsman,isBowler)
+select distinct null,bowler,bowling_team,0,1
 from odi_match_data 
 where bowling_team not in 
 ('Papua New Guinea',
@@ -18,6 +19,21 @@ where bowling_team not in
 'Oman',
 'United States of America')
 and wicket_type  is not null and wicket_type not in ('run out','retired hurt','hit wicket', 'obstructing the field')
-group by bowler,bowling_team having count(wicket_type)>25 
+group by bowler,bowling_team having count(wicket_type)>20
 
---select distinct wicket_type from odi_match_data
+--update dupllicate records before removing
+update players set isBatsman = 1, isBowler = 1 where name in(
+select  name from players group by name having count(*)>1
+)
+
+--reseed identity 
+--DELETE FROM SQLITE_-SEQUENCE WHERE NAME = 'players';
+
+--delete duplicates
+DELETE FROM players
+ WHERE id NOT IN
+(
+  SELECT MIN(id) id
+    FROM players
+   GROUP BY name
+)
